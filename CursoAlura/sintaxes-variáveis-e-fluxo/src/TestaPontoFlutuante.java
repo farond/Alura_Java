bf877fbad37d5f17b74ec030ae9050e9396063ef
Fabio Lino <43458363+farond@users.noreply.github.com>
2021-07-13 17:25:12 -0300

    Curso Java Orientado a Objetos

---
+++ CursoAlura/sintaxes-variáveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,20 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double salario;
+		salario = 1500.70;
+		System.out.println("O salário é " + salario);
+		
+		double divisao = Math.PI / 2;		
+		System.out.println(divisao);
+		
+		int outraDivisao = 5/2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0/2;
+		System.out.println(novaTentativa);
+	}
+
+}
